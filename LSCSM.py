3619736a91331749006de184c0d5aa0c8784faa8
mlarroche <margot.larroche@unic.cnrs-gif.fr>
2015-09-17 14:05:48 +0200

    Deleted obsolete meta-parameter LL, transformed hlsr (=ratio) into num_hidden (= number of cells in hidden layer), added meta-parameters for bounds

--- LSCSM.py
+++ LSCSM.py
@@ -12,31 +12,34 @@ from TheanoVisionModel import TheanoVisionModel
 class LSCSM(TheanoVisionModel):
       
       num_lgn = param.Integer(default=9,bounds=(0,10000),doc="""Number of lgn units""")
-      hlsr = param.Number(default=0.2,bounds=(0,1.0),doc="""The hidden layer size ratio""")
+      num_hidden = param.Integer(default=20,bounds=(0,10000),doc="""Number of hidden units""")
       v1of = param.String(default='LogisticLoss',doc="""Transfer function of 'V1' neurons""")
       lgnof = param.String(default='Linear',doc="""Transfer function of 'LGN' neurons""")
       balanced_LGN = param.Boolean(default=False,doc="""Will all LGN cells have balanced strength of center and surround?""")
       LGN_treshold = param.Boolean(default=False,doc="""Will all LGN cells have threshold?""")
       second_layer = param.Boolean(default=True,doc="""Will there be a second layer of neurons?""")
-      LL = param.Boolean(default=True,doc="""Whether to use Log-Likelyhood. False will use MSE.""")
       negative_lgn = param.Boolean(default=True,doc="""Whether LGN neurons can send negative weights to L4""")
       maximum_weight_l1 = param.Number(default=20000,bounds=(0,100000000000000),doc="""Maximum weights in the hidden layer""")
       maximum_weight_l2 = param.Number(default=4,bounds=(0,100000000000000),doc="""Maximum weights in the output layer""")
+      lgn_pos_bound = param.Number(default=2,bounds=(0,100),doc="""Minimum distance from LGN RF center to image edge""")
+      lgn_size_bounds = param.NumericTuple(default=(1,25),length=2,doc="""LGN RF size bounds""")
+      lgn_gain_bounds = param.NumericTuple(default=(0.0,10.0),length=2,doc="""LGN center and surround amplitude bounds""")
+      threshold_bounds = param.NumericTuple(default=(-20,20),length=2,doc="""Spiking threshold bounds for all layers""")      
       
       def construct_free_params(self):
           
             # LGN 
-            self.lgn_x = self.add_free_param("x_pos",self.num_lgn,(6,self.size-6))
-            self.lgn_y = self.add_free_param("y_pos",self.num_lgn,(6,self.size-6))
-            self.lgn_sc = self.add_free_param("size_center",self.num_lgn,(1,25))
-            self.lgn_ss = self.add_free_param("size_surround",self.num_lgn,(1,25))
+            self.lgn_x = self.add_free_param("x_pos",self.num_lgn,(self.lgn_pos_bound,self.size-self.lgn_pos_bound))
+            self.lgn_y = self.add_free_param("y_pos",self.num_lgn,(self.lgn_pos_bound,self.size-self.lgn_pos_bound))
+            self.lgn_sc = self.add_free_param("size_center",self.num_lgn,self.lgn_size_bounds)
+            self.lgn_ss = self.add_free_param("size_surround",self.num_lgn,self.lgn_size_bounds)
             
             if not self.balanced_LGN:
-               self.lgn_rc = self.add_free_param("center_weight",self.num_lgn,(0.0,10.0))
-               self.lgn_rs = self.add_free_param("surround_weight",self.num_lgn,(0.0,10.0))
+               self.lgn_rc = self.add_free_param("center_weight",self.num_lgn,self.lgn_gain_bounds)
+               self.lgn_rs = self.add_free_param("surround_weight",self.num_lgn,self.lgn_gain_bounds)
 
             if self.LGN_treshold:
-                self.lgn_t = self.add_free_param("lgn_threshold",self.num_lgn,(0,20))
+                self.lgn_t = self.add_free_param("lgn_threshold",self.num_lgn,self.threshold_bounds)
                 
             # V1
             if self.negative_lgn:
@@ -45,17 +48,18 @@ class LSCSM(TheanoVisionModel):
                minw = 0
 
             if self.second_layer:
-               self.hidden_w = self.add_free_param("hidden_weights",(self.num_lgn,int(self.num_neurons*self.hlsr)),(minw,self.maximum_weight_l1))
-               self.hl_tresh = self.add_free_param("hidden_layer_threshold",int(self.num_neurons*self.hlsr),(0,20))
-               self.output_w = self.add_free_param("output_weights",(int(self.num_neurons*self.hlsr),self.num_neurons),(-self.maximum_weight_l2,self.maximum_weight_l2))
-               self.ol_tresh = self.add_free_param("output_layer_threshold",int(self.num_neurons),(0,20))
+               self.hidden_w = self.add_free_param("hidden_weights",(self.num_lgn,self.num_hidden),(minw,self.maximum_weight_l1))
+               self.hl_tresh = self.add_free_param("hidden_layer_threshold",self.num_hidden,self.threshold_bounds)
+               self.output_w = self.add_free_param("output_weights",(self.num_hidden,self.num_neurons),(-self.maximum_weight_l2,self.maximum_weight_l2))
+               self.ol_tresh = self.add_free_param("output_layer_threshold",self.num_neurons,self.threshold_bounds)
             else:
                self.output_w = self.add_free_param("output_weights",(self.num_lgn,self.num_neurons),(minw,self.maximum_weight_l1))
-               self.ol_tresh = self.add_free_param("output_layer_threshold",self.num_neurons,(0,20))
+               self.ol_tresh = self.add_free_param("output_layer_threshold",self.num_neurons,self.threshold_bounds)
 
 
       def construct_model(self):
             # construct the 'retinal' x and y coordinates matrices
+            # = list of x-coordinates and list of y-coordinates for each position in the grid
             xx = theano.shared(numpy.repeat([numpy.arange(0,self.size,1)],self.size,axis=0).T.flatten())   
             yy = theano.shared(numpy.repeat([numpy.arange(0,self.size,1)],self.size,axis=0).flatten())
             
